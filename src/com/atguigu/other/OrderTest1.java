632ac98d80c58fe7d49dd67162c653a751c7e990
琮神 <2068228445@qq.com>
2021-01-28 22:14:41 +0800

    update src/com/atguigu/other/OrderTest1.java.

--- src/com/atguigu/other/OrderTest1.java
+++ src/com/atguigu/other/OrderTest1.java
@@ -20,6 +20,6 @@ public class OrderTest1 {
             System.out.print(o+" ");
         }
         System.out.println("你好123");
-        System.out.println("你好225");
+        System.out.println("你好2299");
     }
 }
